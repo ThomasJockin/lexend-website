5bd122b72f517a04e3537dcfd8ccb4d4a3688d38
micah rich <micah@micahrich.com>
2019-06-03 21:36:47 -0400

    favicon

--- components/Layout.js
+++ components/Layout.js
@@ -41,6 +41,16 @@ export default ({ children }) => {
       />
       <Head>
         <title>Lexend — A Font for Reading</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/static/favicon/site.webmanifest"/>
+        <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+        <link rel="shortcut icon" href="/static/favicon/favicon.ico"/>
+        <meta name="msapplication-TileColor" content="#b91d47"/>
+        <meta name="msapplication-config" content="/static/favicon/browserconfig.xml"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
       <Nav>
         <Logo/>
